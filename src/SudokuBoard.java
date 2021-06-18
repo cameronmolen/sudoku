4c043381a99d233135b2023ee3b38e98909a53bd
Cameron Molen <cameronmolen@gmail.com>
2021-06-18 13:54:46 -0600

    Added methods to check if row, column, and box are valid

--- src/SudokuBoard.java
+++ src/SudokuBoard.java
@@ -22,6 +22,10 @@ public class SudokuBoard {
     generateOtherMatrices(values);
 
     System.out.println(toString());
+
+    // TESTING
+//    System.out.println("At board[3][0], 4 is a valid number: " + validCell(3, 0, 4));
+    // END TESTING
   }
 
   /**
@@ -47,6 +51,64 @@ public class SudokuBoard {
    */
   private void generateOtherMatrices(ArrayList<Integer> values) {
 
+  }
+
+  /**
+   * Checks if a cell is valid (no other instances of the value appear in the same row, column, or box).
+   * @param row the row of the cell
+   * @param col the column of the cell
+   * @param val the value of the cell to be checked
+   * @return true if the cell is valid
+   */
+  private boolean validCell(int row, int col, int val) {
+    if(board[row][col] != 0) return false;
+    return validRow(row, val) && validCol(col, val) &&
+            validBox(row - (row % BOX_DIMENSIONS), col - (col % BOX_DIMENSIONS), val);
+  }
+
+  /**
+   * Checks if a row contains the specified value and returns true if the row is valid (no other
+   * instances of the value appear).
+   * @param row row to check
+   * @param val value of the cell
+   * @return true if the row is valid
+   */
+  private boolean validRow(int row, int val) {
+    for(int col = 0; col < BOARD_DIMENSIONS; col++) {
+      if(board[row][col] == val) return false;
+    }
+    return true;
+  }
+
+  /**
+   * Checks if a column contains the specified value and returns true if the column is valid (no other
+   * instances of the value appear).
+   * @param col column to check
+   * @param val value of the cell
+   * @return true if the column is valid
+   */
+  private boolean validCol(int col, int val) {
+    for(int row = 0; row < BOARD_DIMENSIONS; row++) {
+      if(board[row][col] == val) return false;
+    }
+    return true;
+  }
+
+  /**
+   * Checks if a box contains the specified value and returns true if the box is valid (no other instances
+   * of the value appear in the box).
+   * @param startingRow the row at which the box begins (top left corner)
+   * @param startingCol the column at which the box begins (top left corner)
+   * @param val value of the cell
+   * @return true if the box is valid
+   */
+  private boolean validBox(int startingRow, int startingCol, int val) {
+    for(int row = startingRow; row < startingRow + BOX_DIMENSIONS; row++) {
+      for(int col = startingCol; col < startingCol + BOX_DIMENSIONS; col++) {
+        if(board[row][col] == val) return false;
+      }
+    }
+    return true;
   }
 
   /**
